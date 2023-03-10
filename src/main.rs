fb4c27b80a3a7f75441b685941237b721ff0a510
Dwaipayan Dehury <dwaipayandehury@Dwaipayans-MacBook-Air.local>
2023-03-10 08:50:10 +0530

    rust 1st

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+use std::io;
+fn main() {
+    // let mut buffer = String::new();
+    // println!("Enter a message");
+    // io::stdin().read_line(&mut buffer);
+    // println!("Buffer is {}",buffer);
+
+    let number=rand::random::<f64>();
+    println!("number is {}", number);
+}
